6c7a9c474e377d7d34a9e65d7ad909eefee78bbe
Maja G <maja293@outlook.com>
2022-10-01 16:23:54 +0100

    Medians task complete

--- median.py
+++ median.py
@@ -1,12 +1,26 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+count = 0
+numbers = []
 
+def median_func(numbers, count):
+    if count % 2 != 0:
+        return(numbers[(count//2)])
+    else:
+        median = (numbers[count//2]+numbers[(count//2)-1])/2
+        return(median)
+
+        
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        for value in input().split(","):
+            numbers.append(float(value))
+            count = count + 1
+        finalMedian = median_func(numbers, count)
     except ValueError:
         print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+    finally:
+        print(numbers)
+        print(count)
+        print(median_func(numbers, count))
